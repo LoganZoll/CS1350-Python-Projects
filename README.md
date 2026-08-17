1c9f991220952b630ff32a0b26b2b1c011b49ac5
LoganZoll <Lazoll01@indianatech.net>
2026-08-17 14:16:32 -0400

    Initial commit - first project

---
+++ README.md
@@ -0,0 +1,6 @@
+# My Python Learning Journey
+This repository contains my Python programming exercises and projects.
+## About Me
+- Name: Logan Zoll
+- Course: CS 1350 DAY AB
+- School: Indiana Tech
---
+++ hello.py
@@ -0,0 +1,7 @@
+# My first Python program
+name = input("What's your name? ")
+print(f"Hello, {name}! Welcome to python programming!")
+
+import sys
+print("Python Version check:")
+print(sys.version)
